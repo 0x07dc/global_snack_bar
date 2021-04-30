49aff3e9e2cd21de8954647e8ebb6100d7a372a4
Jamil Voss <voss.jamil@gmail.com>
2021-04-30 17:59:35 -0400

    Updated null safety, dart analysis issues, broadcast streaming, and comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,36 @@
-## [0.0.1] - 4/3/2021
+## [0.0.7] - 4/30/2021
 
-* Initial Release
+* Corrected the change log, CHANGELOG.md
+
+## [0.0.6] - 4/30/2021
+
+* Corrected dart analysis issues and added documentation comments
+
+## [0.0.5] - 4/29/2021
+
+* Fixed multiple listeners issue with broadcast stream
+
+## [0.0.4] - 4/29/2021
+
+* Migrated to null safety and dart 2.12
+
+## [0.0.3] - 4/3/2021
+
+* Updated README.md for package and example.
 
 ## [0.0.2] - 4/3/2021
 
 * Updated first two lines of the LICENSE file, to hopefully have pub.dev display that's it's an MIT license (for convenience).
 
-## [0.0.3] - 4/3/2021
+## [0.0.1] - 4/3/2021
 
-* Updated README.md for package and example.
+* Initial Release
+
+
+
+
+
+
+
+
+
--- lib/global_snack_bar.dart
+++ lib/global_snack_bar.dart
@@ -8,13 +8,15 @@ import 'package:flutter/material.dart';
 /// Must be a child of a scaffold
 /// Simply wrap the content with [GlobalMsgWrapper]
 class GlobalMsgWrapper extends StatefulWidget {
-  Widget content;
+  /// The content that will appear from this widget
+  final Widget content;
   GlobalMsgWrapper(this.content);
 
   @override
   _GlobalMsgWrapperState createState() => _GlobalMsgWrapperState();
 }
 
+/// The state of the [GlobalMsgWrapper] Widget
 class _GlobalMsgWrapperState extends State<GlobalMsgWrapper> {
 
   @override
@@ -32,6 +34,7 @@ class _GlobalMsgWrapperState extends State<GlobalMsgWrapper> {
     super.dispose();
   }
 
+  /// Shows the snackbar on any [GlobalSnackBar] widgets
   void showSnackBar(GlobalMsg msg) {
     SnackBar bar = SnackBar(
       content: Text(msg.text),
@@ -49,21 +52,36 @@ class _GlobalMsgWrapperState extends State<GlobalMsgWrapper> {
   }
 }
 
+/// Provides a model for global messages
 class GlobalMsg {
   GlobalMsg(this.text, {this.bgColor});
+
+  /// Text to show in snackbar
   String text = "";
-  Color bgColor = Colors.teal;
+
+  /// Background color of snackbar
+  Color? bgColor = Colors.teal;
 }
 
+/// Provides the stream logic for the [GlobalSnackBar]
 class GlobalSnackBarBloc {
+  /// Create a new [streamController] to handle broadcasting to a [Stream]
   static final streamController =
-      StreamController(); // create a StreamController or
+      StreamController.broadcast(); // create a StreamController or
   // final counterController = PublishSubject() or any other rxdart option;
+
+  /// Provides a [Stream] for new messages
   static Stream get newMessage =>
       streamController.stream; // create a getter for our Stream
   // the rxdart stream controllers returns an Observable instead of a Stream
 
+  /// Method to show a [SnackBar] message on any [GlobalSnackBar] Widgets
   static void showMessage(GlobalMsg data) {
     streamController.sink.add(data); // add whatever data we want into the Sink
+  }
+
+  /// Allows for the stream to be closed
+  dispose(){
+    streamController.close();
   }
 }
